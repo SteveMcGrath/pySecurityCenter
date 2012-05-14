cb5681a6dbfb4414fda33a611631073e86a07823
Steven McGrath <steve@chigeek.com>
2012-05-14 10:13:40 -0500

    Added LCE Unique Processes Example

---
+++ examples/lce_unique_procs/lce_unique_procs.py
@@ -0,0 +1,39 @@
+import securitycenter
+import time
+import re
+
+username = 'USERNAME'
+password = 'PASSWORD'
+hostname = 'HOSTNAME'
+days = 7
+
+sc = securitycenter.SecurityCenter(hostname, username, password)
+
+queries = [{
+    'eventName': 'Unique_Windows_Executable',
+    'regex': re.compile(r'invoked \'(.*?)\''),
+    'regex_type': 'single',
+    },{
+    'eventName': 'Daily_Command_Summary',
+    'regex': re.compile(r'day: (.*?) \('),
+    'regex_type': 'multiple',
+    }
+]
+
+procs = set()
+
+for query in queries:
+    data = sc.query('syslog', source='lce', 
+                    eventName=query['eventName'],
+                    endtime=int(time.time()),
+                    starttime=(int(time.time()) - (86400 * days))
+                   )
+    for item in data:
+        values = query['regex'].findall(item['message'])
+        for value in values:
+            if query['regex_type'] == 'single':
+                procs.add(value)
+            if query['regex_type'] == 'multiple':
+                for val in value.split(', '):
+                    procs.add(val)
+print '%s:\t%s' % (len(procs), ', '.join(procs))
