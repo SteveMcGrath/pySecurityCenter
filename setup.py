1203d53b7ae98791cdcd825c31009242aca1e1d4
Steven McGrath <smcgrath@tenable.com>
2013-05-29 16:43:54 -0500

    Updated to stable and added needed items in setup.py for 2to3 support.

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ import sys
 # These are the requirements for pySecurityCenter
 requirements = ['poster',]
 
-# If we are running on somethign thats not 2.6 or 2.7, we will need the
+# If we are running on something thats not 2.6 or 2.7, we will need the
 # simplejson module.  Python versions less than 2.6 need specifically version
 # 2.1.0 as newer ones break on python 2.4.  The reason we need simplejson on
 # versions greater than 2.7 is that it seems like the typcasting on the json
 # module in version 3.x is too strict, and is causing a lot of issues.
+extra = {}
 if sys.version_info < (2, 6, 0):
     requirements.append('simplejson==2.1.0')
-if sys.version_info > (3, 0, 0):
+if sys.version_info > (3,):
     requirements.append('simplejson')
-    
+    extra['use_2to3'] = True
 
 setup(
     name='pySecurityCenter',
-    version='0.3.7',
+    version='0.3.8',
     description='Security Center 4 API Module',
     author='Steven McGrath',
     author_email='smcgrath@tenable.com',
@@ -25,13 +26,16 @@ setup(
     py_modules=['securitycenter'],
     install_requires=requirements,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.4',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
-    ]
+    ],
+    **extra
 )
