6eccb0d3cd69a41d4b6362769ef04304007eb88e
David Lord <david.lord@moesol.com>
2013-08-09 10:37:29 -0700

    document core connection

--- securitycenter/core.py
+++ securitycenter/core.py
@@ -7,15 +7,42 @@ from .exc import raise_for_error, CoreError
 
 
 class SecurityCenter(object):
+    """Open a connection to a Security Center instance.  Modules are
+    registered on the connection object for performing actions.
+
+    If a username and password are provided, they will be used to
+    log in after connecting.
+
+    SSL is possible by passing a client certificate and a trust chain.
+    When using two-way SSL with a client certificate, ``system.init``
+    can cause an automatic log in if the certificate is registered.
+    This can be disabled by setting ``_system_init`` to False.
+
+    :param url: connect to a Security Center instance at this location.
+            Must include scheme (https) and can include port (:443).
+    :param username: after connecting, log in as this user, if given
+    :param password: used for log in with username
+    :param cert: path to client certificate for two-way SSL.  Can be a
+            tuple of (cert, key) if they are separate.
+    :param verify: how to verify host SSL cert.  If True, use system
+            trust; if False, don't verify; otherwise, a path to a trust
+            chain file.
+    :param _system_init: whether to call ``system.init`` on connect
+    """
+
     def __init__(self, url, username=None, password=None, cert=None, verify=False, _system_init=True):
+        # true endpoint is "request.php"
         self._url = urljoin(url, "request.php")
 
+        # set up session with SSL
         self._session = Session()
         self._session.cert = cert
         self._session.verify = verify
 
+        # token returned after login
         self._token = None
 
+        # register available modules, passing self as connection
         self.admin = modules.Admin(self)
         self.asset = modules.Asset(self)
         self.auth = modules.Auth(self)
@@ -37,16 +64,33 @@ class SecurityCenter(object):
         self.vuln = modules.Vuln(self)
         self.zone = modules.Zone(self)
 
+        # try automatic login by client cert
         if _system_init:
             self.system.init()
 
+        # try manual login by username
         if username is not None and password is not None:
             self.auth.login(username, password)
 
     def _request(self, module, action, input=None, file=None, parse=True):
+        """Make an API call to the given module::action.
+
+        :param module: name of module on server
+        :param action: name of action in module
+        :param input: any arguments to be passed to the module::action
+        :type input: dict
+        :param file: file data to upload
+        :type file: file
+        :param parse: if False, don't parse response as JSON
+
+        :return: dict containing API response, or ``Response`` if parse
+                is False
+        """
+
         if input is None:
             input = {}
 
+        # clean up input as SC expects it
         processed_input = {}
         for key, value in input.iteritems():
             if value is None:
@@ -58,6 +102,7 @@ class SecurityCenter(object):
 
             processed_input[key] = value
 
+        # make the request, and check for HTTP errors
         r = self._session.post(self._url, {
             "module": module,
             "action": action,
@@ -68,12 +113,16 @@ class SecurityCenter(object):
 
         r.raise_for_status()
 
+        # return response directly if not parsing
         if not parse:
             return r
 
+        # parse json response and check for API errors
         j = r.json()
         raise_for_error(j)
 
+        # only allow "regular" responses through
+        # this could change if I figure out what other types really mean
         if j["type"] != "regular":
             # e.g. sc.plugin.get_plugins(families=[]) returns type="plugins"
             # instead of raising an error that it expects at least one family
