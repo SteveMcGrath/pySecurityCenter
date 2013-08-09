4375168dc1db41c7c9f517fb68a70e40f875b4db
David Lord <david.lord@moesol.com>
2013-08-09 10:50:08 -0700

    document exceptions

--- securitycenter/exc.py
+++ securitycenter/exc.py
@@ -1,4 +1,10 @@
 class APIError(Exception):
+    """Raised when an error is received in an API response.
+
+    :param code: internal error code
+    :param message: error message in response
+    """
+
     def __init__(self, code, message):
         self.code = code
         self.message = message
@@ -11,10 +17,20 @@ class APIError(Exception):
 
 
 class CoreError(Exception):
+    """Raised when something in the Python API wrapper goes wrong."""
     pass
 
 
 def raise_for_error(data):
+    """Check if there was an error in an API response.
+
+    If an error is present, extract it and raise an ``APIError``.
+
+    :param data: API response data
+    :return: None if no error
+    :raise: APIError if error is present
+    """
+
     try:
         code = data["error_code"]
     except KeyError:
@@ -22,6 +38,7 @@ def raise_for_error(data):
 
     message = data["error_msg"]
 
+    # code 0 is an OK response, all others are errors
     if not code:
         return
 
