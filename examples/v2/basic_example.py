892e2fd901f9cf5d4c49dee1b82b7afee60ada78
David Lord <david.lord@moesol.com>
2013-08-27 11:01:19 -0700

    convert some examples to v2

---
+++ examples/v2/basic_example.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# Example Python Security Center Script
+
+from securitycenter import Client
+
+# Provide the login info & security center address here:
+username = 'api_user'
+password = 'secret'
+host = 'https://127.0.0.1'
+cert = None
+verify = False
+
+# Instantiate a Security Center instance and login with the credentials
+# provided
+sc = Client(host, username, password, cert, verify)
+
+# Now to query the api for vulnerabilities that have publicly known exploits
+# with high or critical severity and only look for active vulns (no PVS).
+vulns = sc.vuln.query('vulndetails', exploitAvailable='true',
+                      pluginType='active', severity='3,4')
+
+# If we simply wanted to print the IP & Vulnerability name, we could uncomment
+# this code below and run this...
+#for vuln in vulns:
+#    print vuln['ip'], vuln['pluginName']
+
+# However I was thinking we could generate something a little nicer, so we will
+# run through all of the data and parse it into a tiered dictionary like the
+# example here:
+# ips = {
+#   '127.0.0.1': [{VULNERABILITY DICT}, {VULNERABILITY DICT}],
+#   '127.0.0.2': [{VULNERABILITY DICT}, {VULNERABILITY DICT}],
+# }
+
+# Generate IP dictionary from the vulnerability data...
+ips = {}
+for vuln in vulns:
+    ips.setdefault(vuln['ip'], []).append(vuln)
+
+# Now to print the output to the screen.  This could easily be rewritten to
+# output to a file as well, or even parse it into a CSV file if needed.
+for ip, vulns in ips.iteritems():
+    print 'IP Address: {0}'.format(ip)
+    for vuln in vulns:
+        print '\t{0}'.format(vuln['pluginName'])
+        # We could also output the pluginText as well if we wanted, however it
+        # was too wordy for this example.
+
+# There is a lot more that can be done with the API, and if you need more detail
+# I would highly recommend you look at the API documentation and use this
+# example as a reference.
---
+++ examples/v2/show_assets.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from securitycenter import Client
+
+# Please fill out the information below
+username = 'api_user'
+password = 'secret'
+host = 'https://127.0.0.1'
+cert = None
+verify = False
+
+sc = Client(host, username, password, cert, verify)
+
+repos = sc.repository.init()
+assets = sc.asset.init()
+
+print 'Repositories\n------------'
+for repo in repos:
+    print repo['id'], repo['name']
+print
+print 'Assets\n------'
+for asset in assets:
+    print asset['id'], asset['name']
