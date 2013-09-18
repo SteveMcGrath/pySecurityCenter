a005ee7d9acfbbdcf92b2370e79bb9b43831642a
Michael Aque <michael.aque@gmail.com>
2013-09-18 10:31:23 -0700

    added templates for all actions in alert module

--- securitycenter/v2/modules/alert.py
+++ securitycenter/v2/modules/alert.py
@@ -7,3 +7,27 @@ class Alert(Module):
 	@extract_value('alerts')
 	def init(self):
 		return self._request('init')
+
+	def edit(self):
+		#TODO asset::edit
++        raise NotImplementedError 
+
+	def delete(self):
+		#TODO asset::edit
++        raise NotImplementedError 
+
+	def execute(self):
+		#TODO asset::edit
++        raise NotImplementedError 
+
+	def query(self):
+		#TODO asset::edit
++        raise NotImplementedError 
+
+	def validateAdd(self):
+		#TODO asset::edit
++        raise NotImplementedError 
+
+	def validateEdit(self):
+		#TODO asset::edit
++        raise NotImplementedError 
