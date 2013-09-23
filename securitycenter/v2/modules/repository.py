fc02acadfa675a64a8d17ec476d6cf16b6f04d41
Binh Vu <binhv90@gmail.com>
2013-09-23 13:47:28 -0700

    added TODO for repos

--- securitycenter/v2/modules/repository.py
+++ securitycenter/v2/modules/repository.py
@@ -8,4 +8,44 @@ class Repository(Module):
     def init(self):
         return self._request('init')
 
-    #TODO repository
+    def add(self):
+        #TODO repository::add
+        raise NotImplementedError
+
+    def edit(self):
+        #TODO repository::edit
+        raise NotImplementedError
+
+    #TODO repository: import
+
+    def export(self):
+        #TODO repository::export
+        raise NotImplementedError
+
+    def download(self):
+        #TODO repository::download
+        raise NotImplementedError
+
+    def get_remote_repositories(self):
+        #TODO repository::getRemoteRepositories
+        raise NotImplementedError
+
+    def download_remote_repositories(self):
+        #TODO repository::downloadRemoteRepositories
+        raise NotImplementedError
+
+    def validate_add(self):
+        #TODO repository::validateAdd
+        raise NotImplementedError
+
+    def validate_edit(self):
+        #TODO repository::validateEdit
+        raise NotImplementedError
+
+    def generate_nessus_file(self):
+        #TODO repository::generateNessusFile
+        raise NotImplementedError
+
+    def delete(self):
+        #TODO repository::delete
+        raise NotImplementedError
