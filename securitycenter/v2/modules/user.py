99ee647c7a485880b4718bc118c478c645ef2e6a
Michael Aque <michael.aque@gmail.com>
2013-09-25 13:16:05 -0700

    updated User module

--- securitycenter/v2/modules/user.py
+++ securitycenter/v2/modules/user.py
@@ -8,7 +8,84 @@ class User(Module):
     def init(self):
         return self._request('init')
 
-    #TODO user
+
+    def add(self):
+    	#TODO vuln::download
+        raise NotImplementedError
+
+
+	def edit(self):
+		#TODO vuln::download
+        raise NotImplementedError
+
+
+	def delete(self, *ids):
+		"""Deletes user
+
+		:param ids: list of user ids
+
+		"return: returns response
+
+		"""
+		return self._request('delete',{
+			'users': [{'id':id} for id in ids]
+		} )
+
+
+	def query(self, filters = None, start = None, sort = None, stop = None, 
+				tool = None, password = None):
+		"""Query Users module.
+
+		:param filters: specified user filters
+		:param start: startOffset
+		:param sort: sortField
+		:param stop: endOffset
+		:param tool: query tool
+		:param password: user password
+
+		return: return params used
+		"""
+
+		return self._request('query', {
+			'endOffset': end
+			'startOffset': start
+			'sortField': sort
+			'tool': tool
+			'password': password
+			'filters': [{'id':id} for id in ids]
+			})
+
+
+	def changePassword(self,password):
+		"""Change user's password
+
+		:param password: user's specified password
+
+		:return: returns response
+		"""
+
+		return self._request('password': password)
+
+
+	def getCoverage(self, user):
+		"""Get coverage for user.
+
+		:param user: user's ID
+
+		:return: returns user's coverage
+		"""
+
+		return self._request('userID': user)
+
+
+	def validateAdd(self):[{'id':id} for id in ids]
+		#TODO vuln::validateAdd
+        raise NotImplementedError
+
+
+	def validateEdit(self):
+		#TODO vuln::validateEdit
+        raise NotImplementedError
 
 
 class Admin(User):
