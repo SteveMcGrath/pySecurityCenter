dfe5177df3f1fe57beafd8dbe5e09a2ca8948d2e
Steven McGrath <steve@chigeek.com>
2014-06-04 10:15:37 -0500

    Added Average Time to Mitigate Example

---
+++ examples/average_time_to_mitigate/avg_ttm.py
@@ -0,0 +1,20 @@
+import securitycenter
+from datetime import timedelta
+
+HOST = 'HOSTNAME or IP_ADDRESS'
+USER = 'USERNAME'
+PASS = 'PASSWORD'
+ASSET_ID = 81
+
+def get_ttm(**filters):
+    sc = securitycenter.SecurityCenter(HOST, USER, PASS)
+    data = sc.query('vulndetails', source='patched', **filters)
+    agg = 0
+    for item in data:
+        agg += int(item['lastSeen']) - int(item['firstSeen'])
+    avg = float(agg) / len(data)
+    print 'Average Hours to Mitigate : %d' % int(avg / 3600)
+    print 'Average Days to Mitigate : %s' % int(avg / 3600 / 24)
+
+if __name__ == '__main__':
+    get_ttm(assetID=ASSET_ID, severity='4,3,2,1')
