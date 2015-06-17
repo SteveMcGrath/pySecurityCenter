96628c576162b2a486c90bb61bdba87abbff8c2c
Steven McGrath <smcgrath@tenable.com>
2015-06-17 15:00:48 -0500

    Added Core SC5 API Handler

--- securitycenter/__init__.py
+++ securitycenter/__init__.py
@@ -10,6 +10,6 @@ __authors__ = [
     'Steven McGrath <steve@chigeek.com>', 
     'David Lord <davidism@gmail.com>',
 ]
-__version__ = '2.0a2'
+__version__ = '2.1'
 __url__ = 'https://github.com/SteveMcGrath/pySecurityCenter'
 __description__ = 'Python Interface into Tenable\'s SecurityCenter'
--- securitycenter/base.py
+++ securitycenter/base.py
@@ -1,3 +1,220 @@
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import requests
+import logging
+
+
+class APIError(Exception):
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
+
+    def __str__(self):
+        return repr('[%s]: %s' % (self.code, self.msg))
+
+
+class SecurityCenter5(object):
+    _host = None
+    _port = 443
+    _token = None
+    _ssl_verify = False
+    _cookies = {}
+    verbose = False
+
+    def __init__(self, host, port=443, ssl_verify=False, scheme='https'):
+        '''SecurityCenter 5 API Wrapper
+        This class is designed to handle authentication management for the
+        SecurityCenter 5.x API.  This is by no means a complete model of
+        everything that the API can handle, it is simply meant to be a thin
+        wrapper into the API.  Convenience functions will be added as time
+        passes and there is a desire to develop them.
+
+        For more information, please See Tenable's official API documentation
+        at: https://support.tenable.com/support-center/cerberus-support-center/includes/widgets/sc_api/Scanner.html
+        '''
+        self._host = host
+        self._port = port
+        self._ssl_verify = ssl_verify
+        self._scheme = scheme
+        try:
+            url, headers = self._pre_req('system')
+            kwargs = self._kwarg_builder({})
+            d = requests.get(url, **kwargs).json()
+            self.version = d['response']['version']
+            self.build_id = d['response']['buildID']
+            self.license = d['response']['licenseStatus']
+            self.uuid = d['response']['uuid']
+        except:
+            raise APIError(404, 'Invalid SecurityCenter Instance')
+
+    def _pre_req(self, path, headers={}):
+        if self._token:
+            headers['X-SecurityCenter'] = self._token
+        url = '%s://%s:%s/rest/%s' % (self._scheme, self._host, self._port, path) 
+        return url, headers
+
+    def _resp_error_check(self, response):
+        data = response.json()
+        if data['error_code']:
+            raise APIError(data['error_code'], data['error_msg'])
+        else:
+            return response
+
+    def _kwarg_builder(self, headers={}, **kwargs):
+        kwargs['headers'] = headers
+        kwargs['cookies'] = self._cookies
+        kwargs['verify'] = self._ssl_verify
+        if not self._ssl_verify:
+            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        if self.verbose:
+            print kwargs
+        return kwargs
+
+    def head(self, path, headers={}, **kwargs):
+        '''Calls the specified path with the HEAD method'''
+        url, headers = self._pre_req(path, headers)
+        resp = requests.post(url, **self._kwarg_builder(headers, **kwargs))
+        return self._resp_error_check(resp)  
+
+    def get(self, path, headers={}, **kwargs):
+        '''Calls the specified path with the GET method'''
+        url, headers = self._pre_req(path, headers)
+        resp = requests.get(url, **self._kwarg_builder(headers, **kwargs))
+        return self._resp_error_check(resp)
+
+    def post(self, path, headers={}, **kwargs):
+        '''Calls the specified path with the POST method'''
+        url, headers = self._pre_req(path, headers)
+        resp = requests.post(url, **self._kwarg_builder(headers, **kwargs))
+        return self._resp_error_check(resp)     
+
+    def put(self, path, headers={}, **kwargs):
+        '''Calls the specified path with the PUT method'''
+        url, headers = self._pre_req(path, headers)
+        resp = requests.put(url, **self._kwarg_builder(headers, **kwargs))
+        return self._resp_error_check(resp)
+
+    def patch(self, path, headers={}, **kwargs):
+        '''Calls the specified path with the PATCH method'''
+        url, headers = self._pre_req(path, headers)
+        resp = requests.patch(url, **self._kwarg_builder(headers, **kwargs))
+        return self._resp_error_check(resp)
+
+    def delete(self, path, headers={}, **kwargs):
+        '''Calls the specified path with the DELETE method'''
+        url, headers = self._pre_req(path, headers)
+        resp = requests.delete(url, **self._kwarg_builder(headers, **kwargs))
+        return self._resp_error_check(resp)
+
+    def login(self, user, passwd):
+        '''Logs the user into SecurityCenter and stores the needed token and cookies.'''
+        resp = self.post('token', json={'username': user, 'password': passwd})
+        self._token = resp.json()['response']['token']
+        self._cookies['TNS_SESSIONID'] = resp.cookies['TNS_SESSIONID']
+
+    def logout(self):
+        '''Logs out of SecurityCenter and removed the cookies and token.'''
+        resp = self.delete('token')
+        self.token = None
+        self.cookies = {}
+
+    def analysis(self, *filters, **kwargs):
+        '''Analysis
+        A thin wrapper to handle vuln/event/mobile/log analysis through the API.  This
+        function handles expanding multiple filters and will translate arbitrary arguments
+        into the format that SecurityCenter's analysis call expect them to be in.
+
+        In order to make the filter expander more useful for SecurityCenter5 verses the
+        SecurityCenter4 class, filters are no longer done as kwargs, and instead are done
+        as a list of tuples.  For example, to get the IP Summary of all of the hosts in
+        the 10.10.0.0/16 network, we would make the following call:
+
+        vulns = sc.analysis(('ip','=','10.10.0.0/16'), tool='sumip')
+
+        If multiple filters are desired, then it's simply a matter of entering multiple tuples.
+
+        The Analysis function also has a few functions that are sligly deviated from the API
+        guides.  All of these options are optional, however can significantly change the how
+        the API is being called.
+
+        page         - Default "all"                The current page in the pagination sequence.
+        page_size    - Default 1000                 The page size (number of returned results)
+        page_obj     - Default "return_results"     The class thats called after every API pull.
+        page_kwargs  - Default {}                   Any additional arguments that need to be passed
+                                                    to the page_obj class when instantiated.
+        type         - Default "vuln"               This is the type of data that will be returned.
+                                                    As all of the LCE and Vuln calls have been
+                                                    collapsed into "analysis" in the API, each filter
+                                                    needs to have these specified.  This module does
+                                                    that legwork for you.
+        sourceType   - Default "cumulative"         This is how we specify individual scans, LCE archive
+                                                    silos, and other datasets that stand outside the norm.
+        '''
+        output = []
+        def return_results(**kwargs):
+            return kwargs['resp'].json()['response']['results']
+
+        # These values are commonly used and/or are generally not changed from the default.  
+        # If we do not see them specified by the user then we will need to add these in
+        # for later parsing...
+        if 'page' not in kwargs: kwargs['page'] = 'all'
+        if 'page_size' not in kwargs: kwargs['page_size'] = 1000
+        if 'page_obj' not in kwargs: kwargs['page_obj'] = return_results
+        if 'page_kwargs' not in kwargs: kwargs['page_kwargs'] = {}
+        if 'type' not in kwargs: kwargs['type'] = 'vuln'
+        if 'sourceType' not in kwargs: kwargs['sourceType'] = 'cumulative'
+
+        # New we need to pull out the options from kwargs as we will be using hwargs as
+        # the basis for the query that will be sent to SecurityCenter.
+        opts = {}
+        for opt in ['page', 'page_size', 'page_obj', 'page_kwargs']:
+            opts[opt] = kwargs[opt]
+            del kwargs[opt]
+
+        # If a query option was not set, then we will have to.  The hope here is that
+        # we can make a lot of the pain of building the query pretty easy by simply
+        # accepting tuples of the filters.
+        if 'query' not in kwargs:
+            kwargs['query'] = {
+                'tool': kwargs['tool'],
+                'type': kwargs['type'],
+                'filters': [{'filterName': f[0], 'operator': f[1], 'value': f[2], 'type': kwargs['type']} for f in filters]
+            }
+            del kwargs['tool']
+            if opts['page'] == 'all':
+                kwargs['query']['startOffset'] = 0
+                kwargs['query']['endOffset'] = opts['page_size']
+            else:
+                kwargs['query']['startOffset'] = opts['page'] * opts['page_size']
+                kwargs['query']['endOffset'] = (opts['page'] + 1) * opts['page_size']
+
+        count = 0
+        total_records = opts['page_size']
+        while total_records > count:
+            # Here we actually make the calls.  
+            resp = self.post('analysis', json=kwargs)
+            opts['page_kwargs']['resp'] = resp
+            out = opts['page_obj'](**opts['page_kwargs'])
+            if isinstance(out, list):
+                for item in out:
+                    output.append(item)
+            total_records = int(resp.json()['response']['totalRecords'])
+            if opts['page'] == 'all':
+                count = int(resp.json()['response']['endOffset'])
+                kwargs['query']['startOffset'] = count
+                kwargs['query']['endOffset'] = count + opts['page_size']
+            else:
+                count = total_records
+        return output
+
+
+
+########################### LEGACY CODE ###################################
+# This code will eventually be phased out.  Once Tenable stops supporting #
+# SecurityCenter 4, the 4.x API code will be depricated out.              #
+###########################################################################
+
+
+
 import calendar
 from datetime import date, datetime, timedelta
 import httplib
@@ -40,16 +257,6 @@ try:
 except ImportError:
     ssl = None
 
-class APIError(Exception):
-    def __init__(self, code, msg):
-        self.code = code
-        self.msg = msg
-
-    def __str__(self):
-        return repr('[%s]: %s' % (self.code, self.msg))
-
-class SecurityCenter5(object):
-    pass
 
 class SecurityCenter4(object):
     '''
@@ -1058,3 +1265,140 @@ class SecurityCenter4(object):
         return self.raw_query('repository', 'export', data={
             'id': repo_id
         }, dejson=False)
+
+
+    def asset_create(self, name, items, tag='', description='', atype='static'):
+        '''asset_create_static name, ips, tags, description
+        Create a new asset list with the defined information.
+
+        UN-DOCUMENTED CALL: This function is not considered stable.
+
+        :param name: asset list name (must be unique)
+        :type name: string
+        :param items: list of IP Addresses, CIDR, and Network Ranges
+        :type items: list
+        :param tag: The tag associate to the asset list
+        :type tag: string
+        :param description: The Asset List description
+        :type description: string
+        '''
+        data = {
+            'name': name,
+            'description': description,
+            'type': atype,
+            'tags': tag
+        }
+        if atype == 'static':
+            data['definedIPs'] = ','.join(items)
+        if atype == 'dns':
+            data['type'] = 'dnsname'
+            data['definedDNSNames'] = ' '.join(items)
+        return self.raw_query('asset', 'add', data=data)
+
+
+    def asset_create_combo(self, name, combo, tag='', description=''):
+        '''asset_create_combo name, combination, tag, description
+        Creates a new combination asset list.  Operands can be either asset list
+        IDs or be a nested combination asset list.
+
+        UN-DOCUMENTED CALL: This function is not considered stable.
+
+        AND = intersection
+        OR = union
+        operand = asset list ID or nested combination.
+        operator = intersection or union.
+
+        Example:
+
+        combo = {
+            'operand1': {
+                'operand1': '2',
+                'operand2': '2',
+                'operation': 'union',
+            },
+            'operand2': '3',
+            'operation': 'intersection'
+        }
+
+
+        :param name: Name of the asset list.
+        :type name: string
+        :param combo: dict
+        :param tag: The tag of the asset list.
+        :type tag: string
+        :param description: Description of the asset list.
+        :type description: string
+        '''
+        return self.raw_query('asset', 'add', data={
+            'name': name,
+            'description': description,
+            'type': 'combination',
+            'combinations': combo,
+        })
+
+
+    def risk_rule(self, rule_type, rule_value, port, proto, plugin_id, 
+                  repo_ids, comment='', expires='-1', severity=None):
+        '''accept_risk rule_type, rule_value, port, proto, plugin_id, comment
+        Creates an accept rick rule based on information provided.
+
+        UN-DOCUMENTED CALL: This function is not considered stable.
+
+        :param rule_type: Valid options: ip, asset, all.
+        :type rule_type: string
+        :param rule_value: IP Addresses, or assetID if not the type is not all.
+        :type tule_value: string
+        :param port: Port number
+        :type port: string
+        :param proto: Either the protocol ID or any. TCP = 6, UDP = 17, ICMP = 1
+        :type proto: string
+        :param plugin_id: The plugin ID
+        :type plugin_id: string
+        :param repo_ids: List of repository ids that the rule pertains to.
+        :type repo_ids: string
+        :param comment: General purpose comment field.
+        :type comment: string
+        :param expires: epoch time for expiration.
+        :type expires: string
+        :param severity: New severity rating.
+        '''
+        data = {
+            'hostType': rule_type,
+            'port': port,
+            'comments': comments,
+            'protocol': proto,
+            'pluginID': plugin_id,
+            'repIDs': [{'id': i} for i in repo_ids]
+        }
+        if rule_type != 'all':
+            data['hostValue'] = rule_value
+        if severity is None:
+            data['expires'] = expires
+            return self.raw_query('acceptRiskRule', 'add', data=data)
+        else:
+            sevlevels = {'info': 0, 'low': 1, 'medium': 2, 'high': 3, 'critical': 4}
+            data['severity'] = sevlevels[severity]
+            return self.raw_query('recastRiskRule', 'add', data=data)
+
+
+    def group_add(self, name, restrict, repos, lces=[], assets=[], queries=[],
+                  policies=[], dashboards=[], credentials=[], description=''):
+        '''group_add name, restrict, repos
+        '''
+        return self.raw_query('group', 'add', data={
+            'lces': [{'id': i} for i in lces],
+            'assets': [{'id': i} for i in assets],
+            'queries': [{'id': i} for i in queries],
+            'policies': [{'id': i} for i in policies],
+            'dashboardTabs': [{'id': i} for i in dashboards],
+            'credentials': [{'id': i} for i in credentials],
+            'repositories': [{'id': i} for i in repos],
+            'definingAssets': [{'id': i} for i in restrict],
+            'name': name,
+            'description': description,
+            'users': [],
+            'context': ''
+        })
+
+
+
