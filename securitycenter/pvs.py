db1b40756e6cf8b609d2839712624775c3d1447d
Steven McGrath <smcgrath@tenable.com>
2016-06-28 17:19:53 -0500

    PVS works!

---
+++ securitycenter/pvs.py
@@ -0,0 +1,31 @@
+from .base import BaseAPI, APIError, logging
+
+
+class PVS(BaseAPI):
+    def __init__(self, host, port=8835, ssl_verify=False, scheme='https', log=False):
+        BaseAPI.__init__(self, host, port, ssl_verify, scheme, log)
+
+    def _builder(self, **kwargs):
+        kwargs = BaseAPI._builder(self, **kwargs)
+        return kwargs
+
+    def login(self, username, password):
+        resp = self.post('login', data={
+                'login': username,
+                'password': password,
+                'json': 1,
+        })
+        if resp.status_code == 200:
+            self._token = resp.json()['reply']['contents']['token']
+        else:
+            try:
+                raise APIError(resp.status_code, resp.json())
+            except:
+                print resp.content
+
+    def logout(self):
+        self.post('logout', data={
+            'json': 1,
+            'token': self._token
+        })
+        self._reset_session()
