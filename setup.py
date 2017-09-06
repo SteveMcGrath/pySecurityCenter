c8bf73cee9c7cec81b038cc5d28db8eb3d42a29d
Steven McGrath <smcgrath@tenable.com>
2017-09-06 11:31:37 -0500

    Cleaned out changelog & revved to 3.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name="pySecurityCenter",
-    version='3.0.2',
+    version='3.0.3',
     description="Security Center API Library",
     long_description='Python Interface into Tenable\'s SecurityCenter',
     author='Steven McGrath <steve@chigeek.com>',
